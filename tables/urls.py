ec831d5d66e6dab8873ef0ce123b32491f50788a
learning-log <muhammadkamran2599@gmail.com>
2026-06-16 23:21:56 +0500

    Added registration functionality

--- tables/urls.py
+++ tables/urls.py
@@ -1,6 +1,20 @@
 from django.urls import path
 from .views import home
 
+from .views import (
+    home,
+    register_view,
+    login_view,
+    logout_view
+)
+
 urlpatterns = [
+
     path('', home, name='home'),
+
+    path('register/', register_view, name='register'),
+
+    path('login/', login_view, name='login'),
+
+    path('logout/', logout_view, name='logout'),
 ]
--- tables/views.py
+++ tables/views.py
@@ -1,6 +1,58 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout
+from django.contrib.auth.forms import AuthenticationForm
+
+from .forms import RegisterForm
 # Create your views here.
 
+
+
+
 def home(request):
-    return HttpResponse("Welcome to Todo Application")
+    return render(request, 'home.html')
+
+
+def register_view(request):
+
+    if request.method == 'POST':
+
+        form = RegisterForm(request.POST)
+
+        if form.is_valid():
+            user = form.save()
+
+            login(request, user)
+
+            return redirect('home')
+
+    else:
+        form = RegisterForm()
+
+    return render(request, 'register.html', {'form': form})
+
+
+def login_view(request):
+
+    if request.method == 'POST':
+
+        form = AuthenticationForm(data=request.POST)
+
+        if form.is_valid():
+
+            user = form.get_user()
+
+            login(request, user)
+
+            return redirect('home')
+
+    else:
+        form = AuthenticationForm()
+
+    return render(request, 'login.html', {'form': form})
+
+
+def logout_view(request):
+
+    logout(request)
+
+    return redirect('login')
